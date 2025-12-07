08232e7bffef7b6304799f4b9d6df137f57b727a
arseniy <1919ars@gmail.com>
2025-12-07 13:53:36 +0200

    more

---
+++ example.sh
@@ -0,0 +1,8 @@
+# Number example
+num1=7.7
+num2=10
+sum=$((num1 + num2))
+difference=$((num2 - num1))
+product=$((num1 * num2))
+quotient=$((num2 / num1))
+echo "Sum: $sum, Difference: $difference, Product: $product, Quotient: $quotient"
